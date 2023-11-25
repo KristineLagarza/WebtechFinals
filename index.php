b601a6bc0b586cd7f23f1ae6dea766e27b371bf1
AllanAvila <2191727@slu.edu.ph>
2023-11-25 13:58:22 +0800

    Editted html and css

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
-<?php
-include 'header_sidebar_footer/header.html';
-include 'header_sidebar_footer/sidebar.html';
-include 'header_sidebar_footer/footer.html';
-?>
+<?php include 'header_sidebar_footer/header_NoOptions.html';?>
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-   
+    <link rel="stylesheet" href="\WebtechFinals\stylesheets\index.css">
 
     <title>Singko Video Streaming Log In</title>
 
@@ -15,32 +11,28 @@ include 'header_sidebar_footer/footer.html';
 <body>
 
 
-    <input type="checkbox" id="check">
-    <label for="check" class="icons">
-        <i class='bx bx-menu' id="menu-icon"></i>
-        <i class='bx bx-x' id="close-icon"></i>
-    </label>
-
    
-</header>
 <div class="grid-container">
         <div class="grid-item">
             <div class="text-container">
                 <h1>Video Streaming</h1>
+
+                <div class ="login_container">
                 <form action="login_users.php" method="post">
                 <label for="username">Username:</label><br>
                 <input type="text" id="username" name="username" required><br>
                 <label for="password">Password:</label><br>
                 <input type="password" id="password" name="password" required><br>
 
-            </form>
+                </form>
+            </div>
                 <div class="log-button-container">
                 <a href="login_users.php">Login</a>
                 </div>
             </div>
         </div>
     </div> 
-
+<?php include 'header_sidebar_footer/footer.html';?>
    
 </body>
 </html>
