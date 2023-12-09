b6fd873ee72d2e3ba659577969b1a22b54a34424
Jemma Niduaza <2221083@slu.edu.ph>
2023-12-09 11:39:38 +0800

    viewer

---
+++ viewer/viewer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../stylesheets/style.css">
+        <title>Viewer</title> 
+    </head>
+    <body>
+        <div class="video-container">
+        <div class="video">
+        <video controls width="110%" height="110%">
+                <source src="../contentmanager/lupanghinirang.mp4" type="video/mp4">
+            </video>
+        </div>
+        </div>
+    <?php include ('../header_sidebar_footer/footer.php') ?>
+    </body>
+</html>
