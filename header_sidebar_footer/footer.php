6fd43ce040f03be6f826b5456ac6f5fb25ee5a30
Jemma Niduaza <2221083@slu.edu.ph>
2023-12-09 11:57:46 +0800

    viewer and footer

--- header_sidebar_footer/footer.php
+++ header_sidebar_footer/footer.php
@@ -3,19 +3,24 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Hello World</title>
-
-  <link rel="stylesheet" href="\WebtechFinals\stylesheets\header_footer_sidebar.css">
+  <title>Footer</title>
+  <link rel="stylesheet" href="../stylesheets/header_footer_sidebar.css">
 </head>
 
 <body>
-
-
     <footer>
-      <p>&copy; 2023 Singko VIDEO STREAMING. All rights reserved.</p>
+        <div class="footer-image">
+            <img src="../images/favicon.png" alt="Singko">
+        </div>
+        <h5>&copy;  2023 Singko. All Rights Reserved</h5>
+        <div class="footer-right">
+          <h6>Team Singko - 9481AB - IT312/312L</h6>
+          <h6>AY 2023-2024</h6>
+          <h6>IT Department</h6>
+          <h6>School of Accountancy, Management, Computing and Information Studies</h6>
+          <h6>Saint Louis University</h6>
+        </div>
     </footer>
-    
-  
   </body>
   </html>
   
--- viewer/viewer.php
+++ viewer/viewer.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../stylesheets/style.css">
-        <title>Viewer</title> 
+        <title>View</title> 
     </head>
     <body>
         <div class="video-container">
