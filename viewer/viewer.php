b611d6aa14885c4319bfc7dc1dd1ebce8b0cdfe1
Jemma Niduaza <2221083@slu.edu.ph>
2023-12-14 09:14:29 +0800

    admin css & deleted unused files

--- viewer/viewer.php
+++ viewer/viewer.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../stylesheets/style.css">
-        <title>View</title> 
+        <title>Viewer</title> 
     </head>
     <body>
         <div class="video-container">
@@ -14,6 +14,18 @@
             </video>
         </div>
         </div>
-    <?php include ('../header_sidebar_footer/footer.php') ?>
+        <footer>
+    <div class="footer-image">
+        <img src="images/favicon.png" alt="Singko">
+    </div>
+    <h5>&copy;  2023 Singko. All Rights Reserved</h5>
+    <div class="footer-right">
+          <h6>Team Singko - 9481AB - IT312/312L</h6>
+          <h6>AY 2023-2024</h6>
+          <h6>IT Department</h6>
+          <h6>School of Accountancy, Management, Computing and Information Studies</h6>
+          <h6>Saint Louis University</h6>
+        </div>
+</footer>
     </body>
 </html>
